01253895b2058fe4203b46bde908c06409cd51d9
Nolan Lawson <nolan.lawson@gmail.com>
2016-08-14 11:41:44 -0700

    add --help and -v for --verbose

--- bin.js
+++ bin.js
@@ -3,18 +3,27 @@
 'use strict'
 
 var concat = require('concat-stream')
-var args = require('yargs').argv
 var browserifyCountModules = require('./')
+var yargs = require('yargs')
+var argv = yargs
+  .usage('Usage: $0 [ --verbose ]')
+  .example('browserify index.js | $0', 'count the number of modules')
+  .example('browserify --full-paths index.js | $0 --verbose', 'show a full list of modules')
+  .alias('v', 'verbose')
+  .describe('v', 'Verbose mode')
+  .help('h')
+  .alias('h', 'help')
+  .argv
 
 process.stdin.pipe(concat(function (buf) {
   var str = buf.toString('utf8')
-  browserifyCountModules(str, {verbose: args.verbose}, function (err, res) {
+  browserifyCountModules(str, {verbose: argv.verbose}, function (err, res) {
     if (err) {
       console.error(err)
       process.exit(1)
       return
     }
-    if (args.verbose) {
+    if (argv.verbose) {
       console.log('\nTotal number of modules: ' + res.length + '\n')
       console.log('Modules:')
       console.log(res.map(function (x) { return ' - ' + x }).join('\n') + '\n')
